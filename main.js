a8c05f341d3c17175ea7f3d4e34e00dda23ad36c
shu913 <sss91327@icloud.com>
2023-11-20 12:52:38 +0900

    first commit

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ class App {
 
   load() {
     return new Promise((resolve) => {
-      const earthImgUrl = "public/earth.JPEG";
+      const earthImgUrl = "public/earth.JPG";
       const loader = new THREE.TextureLoader();
       loader.load(earthImgUrl, (texture) => {
         this.texture = texture;
